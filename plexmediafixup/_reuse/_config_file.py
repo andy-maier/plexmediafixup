4d243f29d2bccbfebeec3933f420f3341b8f0af2
Andreas Maier <andreas.r.maier@gmx.de>
2019-12-31 16:40:21 +0100

    Fixup plugin concept and initial fixup for sync_sort_title
    This update imlements the fixup plugin concept, and implements the first
    fixup sync_sort_title to the point where its run() method gets control
    but is not yet implemented.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

---
+++ plexmediafixup/_reuse/_config_file.py
@@ -0,0 +1,131 @@
+"""
+Support for config files in YAML format that are validated using JSON schema.
+"""
+
+from __future__ import print_function, absolute_import
+import errno
+import yaml  # PyYAML package
+import yamlloader
+import jsonschema
+from ._json_schema_defaults import extend_with_default
+
+
+class ConfigFileError(Exception):
+    """
+    An error in the config file.
+    """
+    pass
+
+
+class ConfigFile(object):
+    """
+    A config file in YAML format that is validated using JSON schema.
+    """
+
+    def __init__(self, filepath, json_schema):
+        """
+        Initialize the object. Does not yet load the config file.
+
+        Parameters:
+
+            filepath (string): Path name of config file in YAML format.
+
+            json_schema (dict): JSON schema describing the structure of the
+              config file. See jsonschema package for details.
+        """
+        self._filepath = filepath
+        self._json_schema = json_schema
+        self._data = None
+
+    @property
+    def filepath(self):
+        """
+        string: Path name of the config file.
+        """
+        return self._filepath
+
+    @property
+    def data(self):
+        """
+        dict: Data in the config file, resulting from loading the JSON file into
+        Python objects. Initially None, will be set after calling load().
+        """
+        return self._data
+
+    @property
+    def json_schema(self):
+        """
+        dict: JSON schema of the config file. See jsonschema package for
+        details.
+        """
+        return self._json_schema
+
+    def help(self):
+        """
+        Returns a help text string explaining the structure of the config file,
+        based on its JSON schema.
+        """
+        return "TODO: Create help text from JSON schema"
+
+    def load(self):
+        """
+        Load the config file and set the configuration data in this object.
+
+        Raises:
+            ConfigFileError: Raised for various errors with opening, loading and
+              validating the config file.
+        """
+
+        # Load the config file
+        try:
+            with open(self.filepath) as fp:
+                try:
+                    data = yaml.load(
+                        fp, Loader=yamlloader.ordereddict.CSafeLoader)
+                except (yaml.parser.ParserError,
+                        yaml.scanner.ScannerError) as exc:
+                    raise ConfigFileError(
+                        "Invalid YAML syntax in config file {file}: "
+                        "{exc}: {msg}".
+                        format(file=self.filepath,
+                               exc=exc.__class__.__name__,
+                               msg=exc))
+        except IOError as exc:
+            if exc.errno == errno.ENOENT:
+                raise ConfigFileError(
+                    "Config file not found: {file}".
+                    format(file=self.filepath))
+            else:
+                raise ConfigFileError(
+                    "Config file {file} could not be opened: {msg}".
+                    format(file=self.filepath, msg=exc))
+
+        # Validate config file data against JSON schema
+        ValidatorWithDefaults = extend_with_default(jsonschema.Draft7Validator)
+        validator = ValidatorWithDefaults(self._json_schema)
+        try:
+            validator.validate(data)
+        except jsonschema.exceptions.ValidationError as exc:
+            parm_str = ''
+            for p in exc.absolute_path:
+                # Path contains list index numbers as integers
+                if isinstance(p, int):
+                    parm_str += '[{}]'.format(p)
+                else:
+                    if parm_str != '':
+                        parm_str += '.'
+                    parm_str += p
+            raise ConfigFileError(
+                "Config file {file} contains an invalid item {parm}: {msg} "
+                "(Validation details: Schema item: {schema_item}; "
+                "Failing validator: {val_name}={val_value})".
+                format(
+                    file=self.filepath,
+                    msg=exc.message,
+                    parm=parm_str,
+                    schema_item='.'.join(exc.absolute_schema_path),
+                    val_name=exc.validator,
+                    val_value=exc.validator_value))
+
+        # Persist config file data in this object
+        self._data = data
--- plexmediafixup/cli.py
+++ plexmediafixup/cli.py
@@ -8,8 +8,103 @@ from __future__ import print_function, absolute_import
 import sys
 from pprint import pprint
 import argparse
-from ._smart_formatter import SmartFormatter
-from ._config_file import print_help_config_file, ConfigFile, ConfigFileError
+import json
+import plexapi
+import plexapi.myplex
+import plexapi.exceptions
+from ._reuse._smart_formatter import SmartFormatter
+from ._reuse._config_file import ConfigFile, ConfigFileError
+from ._fixup import FixupManager
+
+
+# JSON schema describing the structure of config files
+CONFIG_FILE_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "http://example.com/root.json",
+    "definitions": {},
+    "type": "object",
+    "title": "plexmediafixup config file",
+    "required": [
+        "servername",
+        "username",
+        "password",
+    ],
+    "additionalProperties": False,
+    "properties": {
+        "servername": {
+            "$id": "#/properties/server",
+            "type": "string",
+            "title": "Server name of the Plex Media Server. This is not the "
+            "IP address or host name, but the server name that can be found "
+            "in the top left corner of the GUI.",
+            "examples": [
+                "MyPlex"
+            ],
+        },
+        "username": {
+            "$id": "#/properties/username",
+            "type": "string",
+            "title": "Username (email) of the Plex Account",
+            "examples": [
+                "john.doe@gmx.de"
+            ],
+        },
+        "password": {
+            "$id": "#/properties/password",
+            "type": "string",
+            "title": "Password of the Plex Account",
+            "examples": [
+                "password"
+            ],
+        },
+        "fixups": {
+            "$id": "#/properties/fixups",
+            "type": "array",
+            "title": "List of fixup items to be executed in list order",
+            "default": [],
+            "items": {
+                "$id": "#/properties/fixups/items",
+                "type": "object",
+                "required": [
+                    "name",
+                    "title",
+                    "enabled",
+                ],
+                "additionalProperties": True,
+                "properties": {
+                    "name": {
+                        "$id": "#/properties/fixups/items/properties/name",
+                        "type": "string",
+                        "title": "Name of the fixup (unique within config "
+                        "file)",
+                        "examples": [
+                            "example_fixup"
+                        ],
+                    },
+                    "title": {
+                        "$id": "#/properties/fixups/items/"
+                        "properties/title",
+                        "type": "string",
+                        "title": "One-line title of the fixup",
+                        "examples": [
+                            "Example fixup"
+                        ],
+                    },
+                    "enabled": {
+                        "$id": "#/properties/fixups/items/"
+                        "properties/enabled",
+                        "type": "boolean",
+                        "title": "Flag controlling whether the fixup is "
+                        "enabled (i.e. will run)",
+                        "examples": [
+                            "true", "false"
+                        ],
+                    },
+                }
+            }
+        }
+    }
+}
 
 
 def create_parser(prog):
@@ -61,18 +156,56 @@ def main():
     argparser = create_parser(prog)
     args = argparser.parse_args()
 
+    config = ConfigFile(args.config_file, CONFIG_FILE_SCHEMA)
+
     if args.help_config:
-        print_help_config_file()
+        print(config.help())
         return 2
 
     try:
-        config_file = ConfigFile(args.config_file)
+        config.load()
     except ConfigFileError as exc:
         print("Error: {}".format(exc))
         return 1
 
-    print("Debug: config_data:")
-    pprint(config_file.data)
+    if args.verbose:
+        print("Loaded config file {file}".
+              format(file=config.filepath))
+
+    servername = config.data['servername']  # required item
+    username = config.data['username']  # required item
+    password = config.data['password']  # required item
+
+    try:
+        plex_account = plexapi.myplex.MyPlexAccount(username, password)
+    except plexapi.exceptions.PlexApiException as exc:
+        print("Error: Cannot login to Plex account {user}: {msg}".
+              format(user=username, msg=exc))
+        return 1
+
+    try:
+        plex_server = plex_account.resource(servername).connect()
+    except plexapi.exceptions.PlexApiException as exc:
+        print("Error: Cannot connect to server {srv} of Plex account {user}: "
+              "{msg}".
+              format(srv=servername, user=username, msg=exc))
+        return 1
+
+    if args.verbose:
+        print("Connected to server {srv} of Plex account {user}".
+              format(srv=servername, user=username))
+
+    fixup_mgr = FixupManager()
+    fixups = config.data.get('fixups', [])
+    for fixup in fixups:
+        name = fixup['name']  # required item
+        # title = fixup['title']  # required item
+        enabled = fixup['enabled']  # required item
+        if enabled:
+            fixup = fixup_mgr.get_fixup(name)
+            if args.verbose:
+                print("Executing fixup {name}".format(name=name))
+            fixup.run(plex_server)
 
     return 0
 
