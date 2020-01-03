d02f702f28f53f052106b1151a4e64aeb111360e
Andreas Maier <andreas.r.maier@gmx.de>
2020-01-03 08:05:28 +0100

    Some reengineering in sync_sort_title fixup incl. move to item.edit()
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- plexmediafixup/fixups/sync_sort_title.py
+++ plexmediafixup/fixups/sync_sort_title.py
@@ -8,6 +8,7 @@ import json
 import six
 import plexapi
 import plexapi.exceptions
+import plexapi.utils
 from plexmediafixup.fixup import Fixup
 from plexmediafixup.utils.unicode import ensure_bytes
 
@@ -23,69 +24,88 @@ class SyncSortTitle(Fixup):
     def run(self, plex, dryrun, verbose, section_types=None,
             section_title_pattern=None, title_pattern=None):
 
-        dryrun_str = "Dryrun: " if dryrun else ""
-
-        if section_types is None:
-            section_types = []
-        elif isinstance(section_types, six.string_types):
+        if isinstance(section_types, six.string_types):
             section_types = [section_types]
 
         for section in plex.library.sections():
 
-            if section.type not in section_types:
+            if section_types is not None and section.type not in section_types:
                 continue
             if section_title_pattern is not None and \
                     re.search(section_title_pattern, section.title) is None:
                 continue
 
             if verbose:
-                print("Processing {s.type} library section: {s.title!r}".
+                print("Processing library section of type {s.type}: "
+                      "{s.title!r}".
                       format(s=section))
 
-            all_items = section.all()
-            for item in all_items:
-
-                if title_pattern is not None and \
-                        re.search(title_pattern, item.title) is None:
-                    continue
-                if not item.title or item.title == item.titleSort:
-                    continue
-
-                if verbose:
-                    print("{d}Syncing sort title field of movie "
-                          "{i.title!r} (was: {i.titleSort!r})".
-                          format(d=dryrun_str, t=type(item), i=item))
-
-                if not dryrun:
-
-                    # Change the sort title field
-                    new_title_sort = item.title
-                    new_title_sort_bytes = ensure_bytes(new_title_sort)
-                    parms = {
-                        'type': 1,  # TODO: Find out what this is
-                        'id': item.ratingKey,
-                        'titleSort.value': new_title_sort_bytes,
-                        'titleSort.locked': 1,
-                        # 'agent': 'com.plexapp.agents.none',
-                    }
-                    # print("Debug: edit() kwargs:")
-                    # print(json.dumps(parms, indent=2))
-                    try:
-                        section.edit(**parms)
-                    except plexapi.exceptions.PlexApiException as exc:
-                        print("Error: Cannot set the sort title to "
-                              "{title!r}: {msg}".
-                              format(title=new_title_sort, msg=exc))
-                        return 1
-
-                    # Verify the sort title field was changed
-                    item.reload()
-                    if item.titleSort != new_title_sort:
-                        print("Error: Attempt to set the sort title to "
-                              "{new_title!r} did not stick, it is still "
-                              "{old_title!r}".
-                              format(new_title=new_title_sort,
-                                     old_title=item.titleSort))
-                        return 1
+            items = section.all()
+            for item in items:
+                if item.type == 'movie':
+                    rc = process_item(section, dryrun, verbose, item,
+                                      title_pattern)
+                    if rc:
+                        return rc
+                elif item.type == 'show':
+                    ep_items = item.episodes()
+                    for ep_item in ep_items:
+                        rc = process_item(section, dryrun, verbose, ep_item,
+                                          title_pattern)
+                        if rc:
+                            return rc
+                else:
+                    print("Error: Invalid section type: {type} for this fixup".
+                          format(type=item.type))
+                    return 1
 
         return 0
+
+
+def process_item(section, dryrun, verbose, item, title_pattern):
+    """
+    Process one movie or episode item
+    """
+    if title_pattern is not None and \
+            re.search(title_pattern, item.title) is None:
+        return 0
+    if not item.title or item.title == item.titleSort:
+        return 0
+
+    dryrun_str = "Dryrun: " if dryrun else ""
+
+    if verbose:
+        print("{d}Syncing sort title field of {i.type} item "
+              "{i.title!r} (was: {i.titleSort!r})".
+              format(d=dryrun_str, i=item))
+
+    if not dryrun:
+
+        # Change the sort title field
+        new_title_sort = item.title
+        new_title_sort_bytes = ensure_bytes(new_title_sort)
+        parm_type = plexapi.utils.SEARCHTYPES[item.type]
+        parms = {
+            'type': parm_type,
+            'id': item.ratingKey,
+            'titleSort.value': new_title_sort_bytes,
+            'titleSort.locked': 1,
+        }
+        try:
+            item.edit(**parms)
+        except plexapi.exceptions.PlexApiException as exc:
+            print("Error: Cannot set the sort title field of "
+                  "{i.type} item to {new_title!r}: {msg}".
+                  format(i=item, new_title=new_title_sort, msg=exc))
+            return 1
+
+        # Verify the sort title field was changed
+        item.reload()
+        if item.titleSort != new_title_sort:
+            print("Error: Attempt to set the sort title field of "
+                  "{i.type} item to {new_title!r} did not stick, "
+                  "it is still {i.titleSort!r}".
+                  format(i=item, new_title=new_title_sort))
+            return 1
+
+    return 0
