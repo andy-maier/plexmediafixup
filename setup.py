5c2b6f9280dd5db5f7c6518633855fe4b754ff8d
Andreas Maier <andreas.r.maier@gmx.de>
2020-01-14 02:58:42 +0100

    Promoted maturity to beta
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setuptools.setup(
     # Keep these Python versions in sync with plexmediafixup/__init__.py
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: Apache Software License',
